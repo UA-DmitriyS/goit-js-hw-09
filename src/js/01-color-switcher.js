9726295db6037bc338dd2a154a09ea4e259ad2c9
UA-DmitriyS <tavluy86@gmail.com>
2023-03-19 15:24:33 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+refs.btnStart.addEventListener('click', startChange);
+refs.btnStop.addEventListener('click', stopChange);
+
+function startChange() {
+  refs.btnStart.disabled = true;
+  timerId = setInterval(
+    () => (refs.body.style.background = getRandomHexColor()),
+    1000
+  );
+}
+
+function stopChange() {
+  clearInterval(timerId);
+  refs.btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
